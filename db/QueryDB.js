40911d07791cf80175d0997df10a9b780875bdbf
Arquimidio <gabrielstoppadossantos@gmail.com>
2023-02-06 20:25:28 -0300

    Create basic functions to QueryDB for testing

--- db/QueryDB.js
+++ db/QueryDB.js
@@ -2,6 +2,23 @@ const DB = require('./DBAccess');
 
 moodule.exports = class QueryDB {
     static getQuotes() {
-        DB.run()
+        return DB.all(
+            `SELECT *
+            FROM quotes`
+        )
+    }
+
+    static postQuote(newQuote) {
+        return DB.run(
+            `INSERT INTO quotes (authorId, quote)
+            VALUES('${newQuote.authorId}', '${newQuote.quote}')`
+        )
+    }
+
+    static postUser(user) {
+        return DB.run(
+            `INSERT INTO users (name, login, password),
+            VALUES('${user.name}', '${user.login}', '${user.password}')`
+        )
     }
 }
--- tests/quotes.test.js
+++ tests/quotes.test.js
@@ -1,6 +1,11 @@
 const request = require('supertest');
 const app = require('../app');
 const requestTest = request(app);
+const QueryDB = require('../db/QueryDB');
+
+beforeAll(async () => {
+    await QueryDB.postUser({ name: "Gabriel", username: "Arquimidio", password: "12345"})
+})
 
 requestTest
     .get()
